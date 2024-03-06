c52062f7792c6a5f54b2359b905d5fa0a8d69f08
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-06 15:55:49 -0800

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a class Square"""
+
+class Square:
+    """instantiates Private instance attribute: size"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError(size must be an int)
+        elif size < 0:
+            raise ValueError(size must be >= 0):
+        self.__size = size
+
+    def area(self):
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
