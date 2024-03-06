309795d010101d25953b7abedd0c41c04d5cb80b
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-06 15:23:01 -0800

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """creates class Square with private instance attribute size"""
 
+
 class Square:
-    """defines class and instantiates private instance attribute size with validation."""
+    """defines class and instantiates private instance attribute size."""
+
 
     def __init__(self, size=0):
         if type(size) is not int:
