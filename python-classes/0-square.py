304544104ee87138c404333c274b7f7e9a6df8d1
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-06 07:43:41 -0800

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ 
+
+
 creating an empty class Square
+
+
 """
 class Square:
     """
-    defining the empty class
-    """
-     pass
+   
+
+   defining the empty class
+   
+
+   """
+   pass
