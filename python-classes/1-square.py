4344f47e0a617b89e84f8c7938649ddfa9ec1243
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-06 08:53:53 -0800

    Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """defines the class Square."""
 
+
 class Square:
     """efines class and instantiates private instance attribute size."""
-
 
     def __init__(self, size):
         self.__size = size
