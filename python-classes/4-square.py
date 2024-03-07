c74814c342bb37b8112a60b92dd9d7c01145586d
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-06 17:47:57 -0800

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,15 +4,15 @@ private instance attribute size and public instance method"""
 
 
 class Square:
-     """defines class with instantiated and validated private instance attribute
+    """defines class with instantiated and validated private instance attribute
 and public instance method."""
-    
-    def __init__(self, Size=0):
+
+    def __init__(self, size=0):
         self.__size = size
-    
+
     @property
     def size(self):
-        return self.__size
+        return(self.__size)
 
     @size.setter
     def size(self, value):
@@ -21,7 +21,7 @@ and public instance method."""
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
-         """calculates and returns current square area"""
-         return(self.__size * self.__size)
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
