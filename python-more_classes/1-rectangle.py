b02927330c807c6980d1341ff5ff5fea3de8dae3
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-09 07:55:28 -0800

    Simple rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-creating a class Rectangle
-"""
+"""creating a class Rectangle"""
 
 
 class Rectangle:
@@ -17,14 +15,14 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-       """manipulating the value of the privide atttribute width"""
-       if not isinstance(value,int):
-           raise TypeError("width must be an integer")
-       if value < 0:
-           raise ValueError("width must be >= 0")
-       self.__width = value
-    
-    @property
+        """manipulating the value of the privide atttribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+        @property
     def height(self):
         """giving access to the private attribute __height"""
         return self.__height
@@ -32,7 +30,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """manipulating height value"""
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
