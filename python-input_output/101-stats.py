3db9cecc6867bca81d6adca77ed98ae7e7987ea1
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-03-16 14:27:46 -0700

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
-"""importing"""
-
-
-import random
+""" reads stdin line by line and computes metrics:
+"""
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+file_size = 0
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+iter = 0
+try:
+    for line in sys.stdin:
+        iter += 1
+        line_list = line.split()
+        if len(line_list) >= 2:
+            if line_list[-2] in status_code:
+                status_code[line_list[-2]] += 1
+            try:
+                file_size += int(line_list[-1])
+            except FileNotFoundError:
+                pass
+
+        if iter % 10 == 0:
+            print(f"File size: {file_size:d}")
+            for key, value in sorted(status_code.items()):
+                if value:
+                    print(f"{key}: {value:d}")
+
+    print(f"File size: {file_size:d}")
+    for key, value in sorted(status_code.items()):
+        if value:
+            print(f"{key}: {value:d}")
+
+except KeyboardInterrupt:
+    print(f"File size: {file_size:d}")
+    for key, value in sorted(status_code.items()):
+        if value:
+            print(f"{key}: {value:d}")
