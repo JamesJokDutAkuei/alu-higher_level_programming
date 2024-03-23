0799cc89306f45e11f94f7ded3cf963048900ac9
JamessJokDutAkuei <j.akuei@alustudent.com>
2024-03-22 18:18:57 -0700

    POST an email #0

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    new_value = {'email': sys.argv[2]}
